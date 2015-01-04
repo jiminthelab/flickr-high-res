6fb68ae80c7bb72e5a09b37d7bcaf3d340ed9e69
Jim in the lab <gerky3@gmail.com>
2015-01-04 20:04:30 +0100

    [Added] Pipe

--- extract.js
+++ extract.js
@@ -3,24 +3,13 @@ var https = require('https');
 var fs = require('fs');
 
 var url = process.argv[2];
-var localDir = './tmp/listdata.html';
-var datastream;
+var writable = fs.createWriteStream('./tmp/listdata.html');
 
 var req = https.get(url, function(res) {
-  res.on('data', function(d) {
-    datastream += d;
-  });
+  res.pipe(writable);
 });
 req.end();
 
 req.on('error', function(e) {
   console.error(e);
 });
-
-// temporary hack, promises yet to come
-setTimeout(function() {
-  fs.writeFile(localDir, datastream, function(err) {
-    if (err) console.log(err);
-    else console.log('Temp file created.');
-  });
-}, 2000);
