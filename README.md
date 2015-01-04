7c72ecd6ea6552a228d2a566959f9aa674d844e7
Jim in the lab <gerky3@gmail.com>
2015-01-04 23:19:39 +0100

    [Updated] Readme

--- README.md
+++ README.md
@@ -6,3 +6,33 @@ by one with the 'save as' method.
 
 This script allows you to download an entire Flickr album's high resolution 
 picture by just inputing the album's URL. 
+
+Flickr's html has a JavaScript variable called Y.listData. All pictures' url are stored inside. What you want to retrive is stored in:
+
+    Y.listData.rows[i].row[j].sizes
+
+There are 11 picture sizes:
+
+* k: large 2048px
+
+* h: large 1600px
+
+* l: Large 1024px
+
+* c: medium 800px
+
+* z: medium 640px
+
+* m: medium 500px
+
+* n: small 320px
+
+* s: small 240px
+
+* t: thumbnail 100px
+
+* q: large square 150px
+
+* sq: square 75px
+
+We are interested in the l format and above.
--- extract.js
+++ extract.js
@@ -1,6 +1,6 @@
-var https = require('https');
-var fs    = require('fs');
-var exec  = require('child_process').exec;
+var https   = require('https');
+var fs      = require('fs');
+var exec    = require('child_process').exec;
 var Promise = require('bluebird');
 
 var url             = process.argv[2];
@@ -8,7 +8,15 @@ var rawDataDir      = './tmp/rawData.html';
 var filteredDataDir = './tmp/filteredData.js';
 var writable        = fs.createWriteStream(rawDataDir);
 
+
+fs.mkdir('tmp', 0777, function(err) {
+  // If the directory doesn't exist, create it. If it exists, use it.
+  console.log('"tmp" directory created.');
+  if (err.code === 'EEXIST') console.log('Using the directory "tmp"');
+});
+
 getUrl().then(function() {
+  // Get the URL, then filter the data.
   exec([
     "sed ",
     "-e '/Y.listData/!d' ",
@@ -20,7 +28,7 @@ getUrl().then(function() {
 function puts(error, stdout, stderr) {
   fs.writeFile(filteredDataDir, stdout, function(err) {
     if (err) console.log(err);
-    else console.log("Temp file created.");
+    else console.log("Data filtered.");
   });
 }
 
@@ -29,13 +37,12 @@ function getUrl() {
 
     var req = https.get(url, function(res) {
       res.pipe(writable);
-      console.log('Temp file created.');
+      console.log('Cloning the album\' code.');
 
       res.on('end', function() {
         resolve(res);
         console.log('Filtering data.');
       });
-
     });
 
     req.end();
