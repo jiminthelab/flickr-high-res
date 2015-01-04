60e139f70283f02debacd88c2e35867499698986
Jim in the lab <gerky3@gmail.com>
2015-01-04 21:35:58 +0100

    [Added] Sed

--- extract.js
+++ extract.js
@@ -1,15 +1,36 @@
-// https is port 443, http is port 80
 var https = require('https');
-var fs = require('fs');
+var fs    = require('fs');
+var exec  = require('child_process').exec;
 
-var url = process.argv[2];
-var writable = fs.createWriteStream('./tmp/listdata.html');
+var url             = process.argv[2];
+var rawDataDir      = './tmp/rawData.html';
+var filteredDataDir = './tmp/filteredData.js';
+var writable        = fs.createWriteStream(rawDataDir);
+
 
 var req = https.get(url, function(res) {
   res.pipe(writable);
+  console.log('Temp file created.');
 });
 req.end();
 
 req.on('error', function(e) {
   console.error(e);
 });
+
+// temp hack
+setTimeout(function() {
+  exec([
+    "sed ",
+    "-e '/Y.listData/!d' ",
+    "-e 's/Y.listData/module.exports.flickr/' ",
+    rawDataDir
+  ].join(''), puts);
+}, 2500);
+
+function puts(error, stdout, stderr) {
+  fs.writeFile(filteredDataDir, stdout, function(err) {
+    if (err) console.log(err);
+    else console.log("Temp file created.");
+  });
+}
