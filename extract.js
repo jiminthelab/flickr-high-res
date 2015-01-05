14c81fd26fabfae4efca2ff527ef62925a721df5
Jim in the lab <gerky3@gmail.com>
2015-01-05 16:21:08 +0100

    [Added] get binaries

--- extract.js
+++ extract.js
@@ -8,24 +8,78 @@ var ensureDir = Promise.promisify(require('fs-extra').ensureDir);
 var url             = process.argv[2];
 var rawDataDir      = './tmp/rawData.html';
 var filteredDataDir = './tmp/filteredData.js';
+var imageList       = [];
 
 ensureDir('tmp')
+  .then(ensureDir('images'))
+  .then(urlExists)
   .then(cloningFlickrAlbum)
-  .then(mkTempFileExportable);
+  .then(mkTempFileExportable)
+  .then(extractImageUrl)
+  .then(initializeDownload);
+
+function initializeDownload() {
+  imageList.forEach(function(element, index) {
+
+    var req = https.get(element, function(res) {
+
+      var fileExtention = element.match(/\.[0-9a-z]+$/i);
+      var imageData = '';
+
+      res.setEncoding('binary');
+
+      res.on('data', function(chunk) {
+        imageData += chunk;
+      });
+
+      res.on('end', function() {
+        fs.writeFile('images/' + index + fileExtention, imageData, 'binary', function(err) {
+          if (err) throw err;
+          console.log('Image ' + index + fileExtention + ' copied.');
+        });
+      });
+
+    });
+  });
+}
+
+function extractImageUrl() {
+  var file = require(filteredDataDir).flickr;
+
+  // All rows
+  file.rows.forEach(function(line) {
+    // In all rows, each row
+    line.row.forEach(function(picture) {
+      if (!!picture.sizes.k) {
+        imageList.push(picture.sizes.k.url);
+      } else if (!!picture.sizes.h) {
+        imageList.push(picture.sizes.h.url);
+      } else if (!!picture.sizes.l) {
+        imageList.push(picture.sizes.l.url);
+      }
+    });
+  });
+}
 
 function mkTempFileExportable() {
-  exec([
-    "sed ",
-    "-e '/Y.listData/!d' ",
-    "-e 's/Y.listData/module.exports.flickr/' ",
-    rawDataDir
-  ].join(''), puts);
+  return new Promise(function(resolve, reject) {
+
+    var execute = exec([
+      "sed ",
+      "-e '/Y.listData/!d' ",
+      "-e 's/Y.listData/module.exports.flickr/' ",
+      rawDataDir
+    ].join(''), puts);
+
+    execute.stdout.on('data', function(data) {
+      resolve(data);
+    });
+  });
 }
 
 function puts(error, stdout, stderr) {
   fs.writeFile(filteredDataDir, stdout, function(err) {
-    if (err) console.log(err);
-    else console.log("Data filtered.");
+    err ? console.log(err) : console.log("Data filtered.")
   });
 }
 
@@ -51,5 +105,11 @@ function cloningFlickrAlbum() {
       reject(e);
     });
 
+  });
+}
+
+function urlExists() {
+  return new Promise(function(resolve, reject) {
+    url ? resolve(url) : console.log('The url is ' + url)
   });
 }
