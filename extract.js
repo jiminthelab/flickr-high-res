3625124177b10c2ff474dd63391d6191c63f4f5b
Jim in the lab <gerky3@gmail.com>
2015-01-05 21:38:28 +0100

    [Update] tree

--- extract.js
+++ extract.js
@@ -1,14 +1,17 @@
 var Promise   = require('bluebird');
 var https     = require('https');
 var exec      = require('child_process').exec;
+var Jetty     = require('Jetty');
 var fs        = Promise.promisifyAll(require('fs'));
 var ensureDir = Promise.promisify(require('fs-extra').ensureDir);
+var ensureFile = Promise.promisify(require('fs-extra').ensureFile);
 
 // TODO: argv compulsory
 var url             = process.argv[2];
 var rawDataDir      = './tmp/rawData.html';
 var filteredDataDir = './tmp/filteredData.js';
 var imageList       = [];
+var jetty           = new Jetty(process.stdout);
 var date            = new Date();
 var albumDir        = ['album',
                        date.getMonth(),
@@ -18,10 +21,6 @@ var albumDir        = ['album',
                        date.getSeconds()
                       ].join('');
 
-var Jetty = require('Jetty');
-var jetty = new Jetty(process.stdout);
-jetty.clear();
-
 ensureDir('tmp')
   .then(ensureDir(albumDir))
   .then(urlExists)
@@ -29,6 +28,87 @@ ensureDir('tmp')
   .then(mkTempFileExportable)
   .then(extractImageUrl)
   .then(initializeDownload);
+
+function urlExists() {
+  return new Promise(function(resolve, reject) {
+    url ? resolve(url) : console.log('The url is ' + url)
+  });
+}
+
+function cloningFlickrAlbum() {
+  return new Promise(function(resolve, reject) {
+    var writable = fs.createWriteStream(rawDataDir);
+
+    var req = https.get(url, function(res) {
+      console.log('Cloning the album\'s code.');
+      res.pipe(writable);
+
+      res.on('end', function() {
+        console.log('Filtering data.');
+        resolve(res);
+      });
+    });
+
+    req.end();
+
+    req.on('error', function(e) {
+      reject(e);
+    });
+
+  });
+}
+
+function mkTempFileExportable() {
+  return new Promise(function(resolve, reject) {
+    var execute = exec([
+      "sed ",
+      "-e '/Y.listData/!d' ",
+      "-e 's/Y.listData/module.exports.flickr/' ",
+      rawDataDir
+    ].join(''), puts);
+
+    execute.stderr.on('err', function(err) {
+      reject(err);
+    });
+
+    setTimeout(function() {
+      resolve();
+    }, 2000);
+  });
+}
+
+function puts(error, stdout, stderr) {
+  fs.writeFile(filteredDataDir, stdout, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Data filtered.");
+      // jetty.clear();
+    }
+  });
+}
+
+function extractImageUrl() {
+  return new Promise(function(resolve, reject) {
+      var file = require(filteredDataDir).flickr;
+
+      // All rows
+      file.rows.forEach(function(line) {
+        // In all rows, each row
+        line.row.forEach(function(picture) {
+          if (!!picture.sizes.k) {
+            imageList.push(picture.sizes.k.url);
+          } else if (!!picture.sizes.h) {
+            imageList.push(picture.sizes.h.url);
+          } else if (!!picture.sizes.l) {
+            imageList.push(picture.sizes.l.url);
+          }
+        });
+      });
+
+      resolve();
+  });
+}
 
 function initializeDownload() {
   return new Promise(function(resolve, reject) {
@@ -63,84 +143,5 @@ function initializeDownload() {
 
       });
     });
-  });
-}
-
-function extractImageUrl() {
-  var file = require(filteredDataDir).flickr;
-
-  // All rows
-  file.rows.forEach(function(line) {
-    // In all rows, each row
-    line.row.forEach(function(picture) {
-      if (!!picture.sizes.k) {
-        imageList.push(picture.sizes.k.url);
-      } else if (!!picture.sizes.h) {
-        imageList.push(picture.sizes.h.url);
-      } else if (!!picture.sizes.l) {
-        imageList.push(picture.sizes.l.url);
-      }
-    });
-  });
-}
-
-function mkTempFileExportable() {
-  return new Promise(function(resolve, reject) {
-
-    var execute = exec([
-      "sed ",
-      "-e '/Y.listData/!d' ",
-      "-e 's/Y.listData/module.exports.flickr/' ",
-      rawDataDir
-    ].join(''), puts);
-
-    execute.stdout.on('data', function(data) {
-      resolve(data);
-    });
-
-    execute.stderr.on('err', function(err) {
-      reject(err);
-    });
-  });
-}
-
-function puts(error, stdout, stderr) {
-  fs.writeFile(filteredDataDir, stdout, function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Data filtered.");
-      jetty.clear();
-    }
-  });
-}
-
-function cloningFlickrAlbum() {
-  return new Promise(function(resolve, reject) {
-
-    var writable = fs.createWriteStream(rawDataDir, {flags: 'w'});
-
-    var req = https.get(url, function(res) {
-      console.log('Cloning the album\'s code.');
-      res.pipe(writable);
-
-      res.on('end', function() {
-        console.log('Filtering data.');
-        resolve(res);
-      });
-    });
-
-    req.end();
-
-    req.on('error', function(e) {
-      reject(e);
-    });
-
-  });
-}
-
-function urlExists() {
-  return new Promise(function(resolve, reject) {
-    url ? resolve(url) : console.log('The url is ' + url)
   });
 }
